c207a67142a4fa0039b01820b12a0da66382cdb3
Mauricio <mauricio.ochoaen@udlap.mx>
2020-08-26 13:17:18 -0500

    26/08/2020

--- expenses_GUI.py
+++ expenses_GUI.py
@@ -1,12 +1,18 @@
 import sqlite3
 import time
-try:
-    import Tkinter as tk
-except:
-    import tkinter as tk
+import tkinter as tk
 from tkinter import *
 
 #Switching to object oriented programming for the GUI frames
+
+#Starting the data bases
+curs = sqlite3.connect('userdata.db')
+conn = sqlite3.connect('expenses.db')
+
+#create a cursor for the database
+cursor = conn.cursor()
+udb_cursor = curs.cursor()
+
 
 class Expense_App(tk.Tk):
     
@@ -16,6 +22,8 @@ class Expense_App(tk.Tk):
         self.switch_frame(Login)
     
     def switch_frame(self, frame_class):
+        self.title("SKDN EXPENSE TRACKER")
+        canvas = self.configure(width = 500, height= 500)
         new_frame = frame_class(self)
         if self._frame is not None:
             self._frame.destroy()
@@ -26,6 +34,7 @@ class Login(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         tk.Label(self, text="Welcome", font=('Helvetica', 18, "bold")).pack(side="top", fill="x", pady=5)
+
 
 if __name__ == "__main__":
     app = Expense_App()
